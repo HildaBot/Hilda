21f07326694986f03c873d3c3eee3d19d2ae2f5b
jamietech <jamie@jamiete.ch>
2017-06-13 17:02:07 +1000

    Ensure that EventHandler annotation is retained at runtime

--- src/ch/jamiete/hilda/events/EventHandler.java
+++ src/ch/jamiete/hilda/events/EventHandler.java
@@ -1,5 +1,14 @@
 package ch.jamiete.hilda.events;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Inherited
 public @interface EventHandler {
 
 }
